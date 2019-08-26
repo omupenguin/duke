4ff143fc6acc8dcbc593756a70c96503c38e97cf
omupenguin <quek.aik.peng@gmail.com>
2019-08-27 00:43:55 +0800

    Changed how the program handles input. Uses a loop instead of recursion now.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
 
     public static ArrayList<String> list = new ArrayList<String>();
 
+    // Print out message with formatting already done
+//    public static void printMsg(ArrayList<String> messages) {
+//        System.out.println("    ____________________________________________________________\n");
+//        for (String msg : messages) {
+//            System.out.println("     " + msg);
+//        }
+//        System.out.println("    ____________________________________________________________\n");
+//    }
+
     public static void startup() {
         String startupMsg = "    ____________________________________________________________\n"
                 + "     Hello! I'm Duke\n"
@@ -16,14 +25,14 @@ public class Duke {
     // Echoes when an item is added
     public static void echo(String inputLine) {
         String echoMsg = "     ____________________________________________________________\n"
-                + "     added: " + inputLine + "\n"
+                + "      added: " + inputLine + "\n"
                 + "     ____________________________________________________________\n";
         System.out.println(echoMsg);
     }
 
     public static void exit() {
         String exitMsg = "     ____________________________________________________________\n"
-                + "     Bye. Hope to see you again soon!\n"
+                + "      Bye. Hope to see you again soon!\n"
                 + "     ____________________________________________________________\n";
         System.out.println(exitMsg);
     }
@@ -36,31 +45,26 @@ public class Duke {
     public static void showList() {
         System.out.println("     ____________________________________________________________");
         for (int i = 0; i < list.size(); i++) {
-            String msg = "     " + (i+1) + ". " + list.get(i);
+            String msg = "      " + (i+1) + ". " + list.get(i);
             System.out.println(msg);
         }
         System.out.println("     ____________________________________________________________");
     }
 
-    public static void handleInput() {
-        Scanner input = new Scanner(System.in);
-        String inputLine = input.nextLine();
+    public static void handleInput(String inputLine) {
         String[] inputArray = inputLine.split(" ");
 
         if (inputArray.length == 1) {
 
-            if (inputLine.equals("bye")) {
-                exit();
-                return;
-            } else if (inputLine.equals("list")) {
+            if (inputLine.equals("list")) {
                 showList();
             } else {
                 addToList(inputLine);
             }
+
         } else {
             addToList(inputLine);
         }
-        handleInput();
     }
 
     public static void main(String[] args) {
@@ -72,7 +76,18 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         startup();
-        handleInput();
+
+        boolean isExit = false;
+        while (isExit == false) {
+            Scanner input = new Scanner(System.in);
+            String inputLine = input.nextLine();
+            if (inputLine.equals("bye")) {
+                isExit = true;
+                exit();
+            } else {
+                handleInput(inputLine);
+            }
+        }
 
     }
 }
